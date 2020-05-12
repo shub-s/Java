0dee930f22d9b6ff4ae9f01dc4eb2010fc8916dc
shubham <shub2495@gmail.com>
2020-05-12 22:54:30 +0530

    Texting Cases Added

---
+++ Demo.java
@@ -0,0 +1,8 @@
+// Save this file as Example.java
+class Example{
+    // Main method starts here
+    public static void main(String[] args) {
+        // Display a single line "World" on the console
+        System.out.println("World");// Printing statement on the console
+    }//main loop ends
+}//class loop ends
