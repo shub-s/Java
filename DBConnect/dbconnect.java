ba09f50e2705d23b189f4eab083a47ec15a3690d
Shubham Sajannavar <shub2495@gmail.com>
2020-05-25 20:51:42 +0530

    JDBC

--- DBConnect/dbconnect.java
+++ DBConnect/dbconnect.java
@@ -3,13 +3,23 @@ import java.sql.*;
 class dbconnect{
     public static void main(String[] args) {
         try {
+            // Register The Driver 
             Class.forName("com.mysql.jdbc.Driver");
+            // Create Connection with Connection Object
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc","root","admin");
+            // Display Status of Connection When Connected
             System.out.println("Connected.");
+
+
+            // Close the Connection
+            conn.close();
             
         } catch (Exception e) {
             System.out.println(e.toString());
+            // Display Error When erroe Occor while connecting
             System.out.println("Faile to Connect! Please check drivers and try again");
+            // Close the Connection
+            conn.close();
         }
     }
 }
