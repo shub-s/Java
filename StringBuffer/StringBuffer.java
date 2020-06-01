0f0ff1d0c7a765cca0c1e5bf0f4fcfa28a41b63e
Shubham Sajannavar <shub2495@gmail.com>
2020-06-01 18:23:16 +0530

    Redesign MountPoints

---
+++ StringBuffer/StringBuffer.java
@@ -0,0 +1,13 @@
+class StringBuffer{
+    public static void main(String[] args) {
+        for(int i=0; i<5; i++){
+            for(int j=0; j<5; j++){
+                System.out.print("i["+i+"]");
+                System.out.print("["+j+"]");
+                System.out.print("    ");
+            }
+            System.out.println("");            
+        }
+
+    }
+}
