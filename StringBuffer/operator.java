ff1cbee40bbcafa37766c59c0865d6c15c973e8c
Shubham Sajannavar <shub2495@gmail.com>
2020-06-03 20:14:50 +0530

    Statement BugFix v0.34

--- StringBuffer/operator.java
+++ StringBuffer/operator.java
@@ -1,7 +1,5 @@
 class operator{
     public static void main(String[] args) {
-        String str = "123";
-        int a = Integer.parseInt(str);
-        System.out.println(a+0);
+        
     }
 }
