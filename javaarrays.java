6887389a82443af59bf49ba45c5c0f73bcc5181f
Shubham Sajannavar <shub2495@gmail.com>
2020-06-06 20:43:04 +0530

    Java Array v0.1

---
+++ javaarrays.java
@@ -0,0 +1,13 @@
+public class javaarrays {
+    public static void main(String[] args) {
+        String[][] rectangle = new String[3][2];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print("A[" + i + "][" + j + "] \t");
+            }
+            System.out.println("");
+        }
+
+    }
+
+}
