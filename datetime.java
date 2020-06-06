3bb8ef24168de920978de7bc2192a089645daf69
Shubham Sajannavar <shub2495@gmail.com>
2020-06-07 00:29:51 +0530

    Date & Time V0.1

---
+++ datetime.java
@@ -0,0 +1,11 @@
+import java.time.*;
+
+class datetime {
+    public static void main(String[] args) {
+        System.out.println("Date and Time");  
+        System.out.println();      
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+        System.out.println(LocalDateTime.now());
+    }    
+}
