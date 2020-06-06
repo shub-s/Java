dce8bef9fc97ee61f86ad19d206371daaffb1f98
Shubham Sajannavar <shub2495@gmail.com>
2020-06-06 20:43:35 +0530

    Numarray JAVA v0.2

---
+++ Garbage/numarray.java
@@ -0,0 +1,10 @@
+class numarray{
+    public static void main(String[] args) {
+        for(int i=0; i<10; i++){
+            if(i>0){
+                System.out.print(" , ");
+            }
+            System.out.print(i);
+        }
+    }
+}
