40991be90ff0fe369e5407c6a75085c146d6c8fe
Shubham Sajannavar <shub2495@gmail.com>
2020-06-19 14:28:57 +0530

    Bank Program

---
+++ InnerClass.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class InnerClass{
+    void test(){
+
+    }
+    class Inner{
+        void display(){
+            System.out.println("Displaying Outer Class");
+        }
+    }
+}
