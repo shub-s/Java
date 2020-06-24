5231f7f257520935e317adf9fbdcffad1340c85a
Shubham Sajannavar <shub2495@gmail.com>
2020-06-24 23:39:09 +0530

    Remening Push

--- ThreadsProgram/src/ThreadProg/SampleThread.java
+++ ThreadsProgram/src/ThreadProg/SampleThread.java
@@ -1,4 +1,34 @@
 package ThreadProg;
+class NewThread implements Runnable {
+    Thread t;
+    NewThread(){
+        t = new Thread(this,"Demo Thread");
+        System.out.println("Child Thread : "+t);
+        t.start(); // Start the threads
+    }
+    @Override
+    public void run() {
+        try {
+            for (int i=0; i<5; i++){
+                System.out.println(i+"Child Running...");
+                Thread.sleep(1000);
+            }
+        }catch (Exception e){
+            System.out.println(e.toString());
+        }
+    }
+}
 
 public class SampleThread {
+    public static void main(String[] args) {
+        new NewThread(); // Create a new Thread
+        try {
+            for (int i=0; i<5; i++){
+                System.out.println(i+"Main Running...");
+                Thread.sleep(100);
+            }
+        }catch (Exception e){
+            System.out.println(e.toString());
+        }
+    }
 }
