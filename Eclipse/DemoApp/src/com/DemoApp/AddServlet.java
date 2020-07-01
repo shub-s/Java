6ac0629ff5969f0c2bbfaf189214cc215f53be0c
Shubham Sajannavar <shub2495@gmail.com>
2020-07-01 13:29:29 +0530

    Arranged Program Files

---
+++ Eclipse/DemoApp/src/com/DemoApp/AddServlet.java
@@ -0,0 +1,17 @@
+package com.DemoApp;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+	public void service(HttpServletRequest req, HttpServletResponse res) {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = i + j;
+		
+		System.out.println("Result is : "+k);
+		
+	}
+}
