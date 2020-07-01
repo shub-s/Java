3c63b5a6c8ebbc2493c5bdbd348a7d906a28074f
Shubham Sajannavar <shub2495@gmail.com>
2020-07-01 23:33:12 +0530

    Program v0.1

---
+++ Eclipse/DemoApp/WebContent/index.jsp
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+<form action="AddNum" method="post">
+	Enter Number <input type="text" name="num1">
+	<br> <br>
+	Enter Number <input type="text" name="num2">
+	<br> <br>
+	<input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
---
+++ Eclipse/DemoApp/src/AddNum.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddNum extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		int a = Integer.parseInt(req.getParameter("num1"));
+		int b = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = a + b;
+		
+		PrintWriter out = res.getWriter();
+		
+		out.println("Result is : "+k);
+	}
+}
